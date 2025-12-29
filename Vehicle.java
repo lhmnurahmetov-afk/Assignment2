57ef0b72847cd78e4bc6dfdae8ae5be302cb5db8
ilkham808 <lhmnurahmetov@gmail.com>
2025-12-30 04:46:26 +0500

    Create abstract Vehicle class with basic properties

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+public abstract class Vehicle {
+
+    protected String brand;
+    protected int year;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public abstract void startEngine();
+    public abstract void stopEngine();
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Year: " + year);
+    }
+}
